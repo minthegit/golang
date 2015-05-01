e4af0839e8b0d7e3c2d1f413bb289b5ab08b07c9
kyungsuk song <gminthe@gmail.com>
2015-05-01 15:16:38 -0700

    test

--- learning/hi.go
+++ learning/hi.go
@@ -2,6 +2,17 @@ package main
 
 import fmt "fmt"
 
+//Page structure
+type Page struct {
+	Title string
+	Body  []byte
+}
+
 func main() {
-  fmt.Print("asd")
+	//fmt.Print("asd")
+
+	k := &Page{Title: "aaaa", Body: nil}
+	k.Title = "kkkk"
+	fmt.Print(k.Title)
+
 }
--- learning/wiki.go
+++ learning/wiki.go
@@ -1,23 +1,54 @@
 package main
 
 import (
-  "fmt"
-  "io/ioutil"
-  "os"
-  )
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+const lenPath = len("/view/")
 
 type Page struct {
-  Title string
-  Body  []byte
+	Title string
+	Body  []byte
 }
 
-func (p *Page) save() os.Error {
-        filename := p.Title + ".txt"
-        return ioutil.WriteFile(filename, p.Body, 0600)
+func (p *Page) save() error {
+	filename := p.Title + ".txt"
+	return ioutil.WriteFile(filename, p.Body, 0600)
 }
 
-func loadPage(title string) *Page {
-        filename := title + ".txt"
-        body, _ := ioutil.ReadFile(filename)
-        return &Page{Title: title, Body: body}
+func loadPage(title string) (*Page, error) {
+	filename := title + ".txt"
+	body, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: title, Body: body}, nil
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[lenPath:]
+	p, _ := loadPage(title)
+	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+}
+
+func main() {
+
+	http.HandleFunc("/view/", viewHandler)
+	http.ListenAndServe(":8080", nil)
+
+	//wiki main
+	/*
+	   p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
+	   //fmt.Println(string(p1))
+
+	   p1.save()
+	   p2, _ := loadPage("TestPage")
+	   fmt.Println(string(p2.Body))
+	*/
 }
